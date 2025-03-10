87739ae227219848b5f7445808ab659272bca08f
ducholmes <84381443+ducholmes@users.noreply.github.com>
2025-03-10 22:27:13 +0700

    Sua title

--- src/main/java/org/example/demo/HelloApplication.java
+++ src/main/java/org/example/demo/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 330, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 330, 250);
+        stage.setTitle("Hello motherfucker");
         stage.setScene(scene);
         stage.show();
     }
