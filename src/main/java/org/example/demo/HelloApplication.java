ae8238bb588a20f91505956403f0e11d6e7618fd
ducholmes <84381443+ducholmes@users.noreply.github.com>
2025-03-10 22:28:29 +0700

    Sua message

--- src/main/java/org/example/demo/HelloApplication.java
+++ src/main/java/org/example/demo/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 330, 250);
+        Scene scene = new Scene(fxmlLoader.load(), 350, 250);
         stage.setTitle("Hello motherfucker");
         stage.setScene(scene);
         stage.show();
--- src/main/java/org/example/demo/HelloController.java
+++ src/main/java/org/example/demo/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("First programm yeh yeh");
     }
 }
