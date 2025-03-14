cbb575cb0b05960e2f56cb1bce54399394207b4a
ducholmes <84381443+ducholmes@users.noreply.github.com>
2025-03-14 16:53:42 +0700

    Add controller and fxml file

--- BroCode/src/main/java/application/brocode/Main.java
+++ BroCode/src/main/java/application/brocode/Main.java
@@ -1,10 +1,13 @@
 package application.brocode;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -13,10 +16,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.rgb(62, 180, 137));
-        stage.setScene(scene);
-        stage.setTitle("First Program");
-        stage.show();
+        URL fxmlResource = getClass().getResource("Main.fxml");;
+        if (fxmlResource == null) {
+            System.out.println("File FXML không được tìm thấy");
+        } else {
+            Parent root = FXMLLoader.load(fxmlResource);
+            Scene scene = new Scene(root, Color.rgb(62, 180, 137));
+            stage.setScene(scene);
+            stage.setTitle("First Program");
+            stage.show();
+        }
     }
 }
